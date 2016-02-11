92117e6a30072653dccd6735424a51b41dbce858
wesslnelson <wesley.nelson@gordon.edu>
2016-02-11 00:56:10 -0500

    Beginning to add more functionality, many broken/unfinished parts still

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,10 +1,32 @@
 var typeCalculator = angular.module('typeCalculator.controllers', ['ionic'])
 
-typeCalculator.controller("TypeCtrl", function($scope) {
+typeCalculator.controller("TypeCtrl", ['$scope', function($scope) {
+  $scope.type1="Bug"
   $scope.typeList = [ "Bug", "Dark", "Dragon", "Electric",
                     "Fairy", "Fighting", "Fire", "Flying",
                     "Ghost", "Ground", "Ice", "Normal",
                     "Poison", "Psychic", "Rock", "Steel",
                     "Water"];
   $scope.typeListCount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-});
+  $scope.weaknessDict = {"Bug":["Fire","Flying","Rock"],
+                         "Dark":["Bug","Fighting", "Fairy"],
+                         "Dragon":["Dragon","Ice","Fairy"],
+                         "Electric":["Ground"],
+                         "Fairy":["Poison", "Steel"],
+                         "Fighting":["Fairy","Flying","Psychic"],
+                         "Fire":["Ground","Rock","Water"],
+                         "Flying":["Electric","Ice","Rock"],
+                         "Ghost":["Dark","Ghost"],
+                         "Ground":["Grass","Ice","Water"],
+                         "Ice":["Fighting","Fire","Rock","Steel"],
+                         "Normal":["Fighting"],
+                         "Poison":["Ground","Psychic"],
+                         "Psychic":["Bug","Dark","Ghost"],
+                         "Rock":["Fighting","Grass","Ground","Steel","Water"],
+                         "Steel":["Fighting","Fire","Ground"],
+                         "Water":["Electric","Grass"]}
+
+  $scope.type1 = function(type) {
+    type1= type;
+  };
+}]);
