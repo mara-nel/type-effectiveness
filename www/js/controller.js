b957bcca99328acf0d5c56e5573b159ff550e831
wesslnelson <wesley.nelson@gordon.edu>
2016-02-12 14:03:10 -0500

    Fully functioning for single types, immunities not included

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,10 +1,10 @@
 var typeCalculator = angular.module('typeCalculator.controllers', ['ionic'])
 
 typeCalculator.controller("TypeCtrl", ['$scope', function($scope) {
-  var type1="Bug"
+  var type1=""
   $scope.typeList = [ "Bug", "Dark", "Dragon", "Electric",
                     "Fairy", "Fighting", "Fire", "Flying",
-                    "Ghost", "Ground", "Ice", "Normal",
+                    "Ghost", "Grass","Ground", "Ice", "Normal",
                     "Poison", "Psychic", "Rock", "Steel",
                     "Water"];
   $scope.typeListCount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
@@ -17,6 +17,7 @@ typeCalculator.controller("TypeCtrl", ['$scope', function($scope) {
                          "Fire":["Ground","Rock","Water"],
                          "Flying":["Electric","Ice","Rock"],
                          "Ghost":["Dark","Ghost"],
+                         "Grass":["Bug","Fire","Flying","Ice","Poison"],
                          "Ground":["Grass","Ice","Water"],
                          "Ice":["Fighting","Fire","Rock","Steel"],
                          "Normal":["Fighting"],
@@ -24,7 +25,28 @@ typeCalculator.controller("TypeCtrl", ['$scope', function($scope) {
                          "Psychic":["Bug","Dark","Ghost"],
                          "Rock":["Fighting","Grass","Ground","Steel","Water"],
                          "Steel":["Fighting","Fire","Ground"],
-                         "Water":["Electric","Grass"]}
+                         "Water":["Electric","Grass"]};
+  //does not include immunities
+  $scope.resistDict = {"Bug":["Fighting","Grass","Ground"],
+                      "Dark":["Dark","Ghost"],
+                      "Dragon":["Electric","Fire","Grass","Water"],
+                      "Electric":["Electric","Flying","Steel"],
+                      "Fairy":["Bug","Dark","Fighting"],
+                      "Fighting":["Bug","Dark","Rock"],
+                      "Fire":["Bug","Fairy","Fire","Grass","Ice","Steel"],
+                      "Flying":["Bug","Fighting","Grass"],
+                      "Ghost":["Bug","Poison"],
+                      "Grass":["Electric","Grass","Ground","Water"],
+                      "Ground":["Poison","Rock"],
+                      "Ice":["Ice"],
+                      "Normal":[],
+                      "Poison":["Bug","Fairy","Fighting","Grass","Poison"],
+                      "Psychic":["Fighting","Psychic"],
+                      "Rock":["Fire","Flying","Normal","Poison"],
+                      "Steel":["Bug","Dragon","Fairy","Flying","Grass","Ice","Normal",
+                    "Psychic","Rock","Steel"],
+                      "Water":["Fire","Ice","Steel","Water"]};
+
 
   $scope.setType1 = function(type) {
     type1= type;
