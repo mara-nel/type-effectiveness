b5127b931508f98e10d5305abb3e23a0b7e358ec
wesslnelson <wesley.nelson@gordon.edu>
2016-02-13 10:36:43 -0500

    Added symbolic constants to replace the constant use of strings as types

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,50 +2,55 @@ var typeCalculator = angular.module('typeCalculator.controllers', ['ionic'])
 
 typeCalculator.controller("TypeCtrl", ['$scope', function($scope) {
   var type1=""
-  $scope.typeList = [ "Bug", "Dark", "Dragon", "Electric",
-                    "Fairy", "Fighting", "Fire", "Flying",
-                    "Ghost", "Grass","Ground", "Ice", "Normal",
-                    "Poison", "Psychic", "Rock", "Steel",
-                    "Water"];
+  //adding some symbolic constants
+  var bug="Bug"; var dark="Dark"; var dragon="Dragon"; var elec="Electric";
+  var fairy="Fairy"; var fight = "Fighting"; var fire="Fire"; var fly="Flying";
+  var ghost="Ghost"; var grass="Grass"; var ground="Ground"; var ice="Ice";
+  var norm="Normal"; var pois="Poison"; var psych="Psychic"; var rock="Rock";
+  var steel="Steel"; var water="Water";
+  
+  $scope.typeList = [ bug, dark, dragon, elec, fairy, fight,
+                      fire, fly, ghost, grass,ground, ice,
+                      norm, pois, psych, rock, steel, water];
   $scope.typeListCount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-  $scope.weaknessDict = {"Bug":["Fire","Flying","Rock"],
-                         "Dark":["Bug","Fighting", "Fairy"],
-                         "Dragon":["Dragon","Ice","Fairy"],
-                         "Electric":["Ground"],
-                         "Fairy":["Poison", "Steel"],
-                         "Fighting":["Fairy","Flying","Psychic"],
-                         "Fire":["Ground","Rock","Water"],
-                         "Flying":["Electric","Ice","Rock"],
-                         "Ghost":["Dark","Ghost"],
-                         "Grass":["Bug","Fire","Flying","Ice","Poison"],
-                         "Ground":["Grass","Ice","Water"],
-                         "Ice":["Fighting","Fire","Rock","Steel"],
-                         "Normal":["Fighting"],
-                         "Poison":["Ground","Psychic"],
-                         "Psychic":["Bug","Dark","Ghost"],
-                         "Rock":["Fighting","Grass","Ground","Steel","Water"],
-                         "Steel":["Fighting","Fire","Ground"],
-                         "Water":["Electric","Grass"]};
+  $scope.weaknessDict = {"Bug":[fire,fly,rock],
+                         "Dark":[bug,fight, fairy],
+                         "Dragon":[dragon,ice,fairy],
+                         "Electric":[ground],
+                         "Fairy":[pois, steel],
+                         "Fighting":[fairy,fly,psych],
+                         "Fire":[ground,rock,water],
+                         "Flying":[elec,ice,rock],
+                         "Ghost":[dark,ghost],
+                         "Grass":[bug,fire,fly,ice,pois],
+                         "Ground":[grass,ice,water],
+                         "Ice":[fight,fire,rock,steel],
+                         "Normal":[fight],
+                         "Poison":[ground,psych],
+                         "Psychic":[bug,dark,ghost],
+                         "Rock":[fight,grass,ground,steel,water],
+                         "Steel":[fight,fire,ground],
+                         "Water":[elec,grass]};
   //does not include immunities
-  $scope.resistDict = {"Bug":["Fighting","Grass","Ground"],
-                      "Dark":["Dark","Ghost"],
-                      "Dragon":["Electric","Fire","Grass","Water"],
-                      "Electric":["Electric","Flying","Steel"],
-                      "Fairy":["Bug","Dark","Fighting"],
-                      "Fighting":["Bug","Dark","Rock"],
-                      "Fire":["Bug","Fairy","Fire","Grass","Ice","Steel"],
-                      "Flying":["Bug","Fighting","Grass"],
-                      "Ghost":["Bug","Poison"],
-                      "Grass":["Electric","Grass","Ground","Water"],
-                      "Ground":["Poison","Rock"],
-                      "Ice":["Ice"],
+  $scope.resistDict = {"Bug":[fight,grass,ground],
+                      "Dark":[dark,ghost],
+                      "Dragon":[elec,fire,grass,water],
+                      "Electric":[elec,fly,steel],
+                      "Fairy":[bug,dark,fight],
+                      "Fighting":[bug,dark,rock],
+                      "Fire":[bug,fairy,fire,grass,ice,steel],
+                      "Flying":[bug,fight,grass],
+                      "Ghost":[bug,pois],
+                      "Grass":[elec,grass,ground,water],
+                      "Ground":[pois,rock],
+                      "Ice":[ice],
                       "Normal":[],
-                      "Poison":["Bug","Fairy","Fighting","Grass","Poison"],
-                      "Psychic":["Fighting","Psychic"],
-                      "Rock":["Fire","Flying","Normal","Poison"],
-                      "Steel":["Bug","Dragon","Fairy","Flying","Grass","Ice","Normal",
-                    "Psychic","Rock","Steel"],
-                      "Water":["Fire","Ice","Steel","Water"]};
+                      "Poison":[bug,fairy,fight,grass,pois],
+                      "Psychic":[fight,psych],
+                      "Rock":[fire,fly,norm,pois],
+                      "Steel":[bug,dragon,fairy,fly,grass,ice,norm,
+                    psych,rock,steel],
+                      "Water":[fire,ice,steel,water]};
 
 
   $scope.setType1 = function(type) {
